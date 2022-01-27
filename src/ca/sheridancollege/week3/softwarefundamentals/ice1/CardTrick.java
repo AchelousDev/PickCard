44bbdf484a4214acacf2a9b4aabebdc92447d8bf
Achelous <fanzhe@sheridancollege.ca>
2022-01-28 04:52:06 +0800

    Finished CardTrick

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -10,20 +10,37 @@ package ca.sheridancollege.week3.softwarefundamentals.ice1;
  * and then asks the user to pick a card and searches the array of cards
  * for the match to the user's card. To be used as starting code in ICE 1
  * @author dancye
- * @modified by Megha Patel
+ * @modified by Zhenqian Fan
  * 
  */
+import java.util.Random;
+import java.util.Scanner;
 public class CardTrick {
     
     public static void main(String[] args)
     {
-        Card[] magicHand = new Card[7];
+        Scanner input = new Scanner(System.in);
+        Random card = new Random();
+        int[] magicHand = new int[7];
+        
+        System.out.print("Enter number from 1 - 13: ");
+        int n = input.nextInt();
+        //System.out.println(n);
         
         for (int i=0; i<magicHand.length; i++)
         {
-            Card c = new Card();
-             }
+            magicHand[i] = card.nextInt(13) + 1;
+            System.out.println(magicHand[i] + " ");
+            if (n == magicHand[i]) {
+                System.out.println("    - Right Choice " + "(" + magicHand[i] + ")");
+            }
+            
+        }
         
-         }
+        
+        
+         
+    }   
+    
     
 }
